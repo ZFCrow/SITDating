bcb10dd44af2f7e40ec092a97d5c8619e3de9a1e
ZFCrow <2302623@sit.singaporetech.edu.sg>
2024-06-23 12:07:06 +0800

    hello

---
+++ constants.py
@@ -0,0 +1,12 @@
+
+# SSH server details
+ssh_host = "35.212.133.154"
+ssh_username = "sitdate-dev"
+ssh_password = "sitokc"  # SSH password
+
+# MySQL server details
+db_host = "localhost"
+db_user = "sitdate-sqldev"
+db_password = "sit123"
+db_name = "sit_date"
+db_port = 3306
